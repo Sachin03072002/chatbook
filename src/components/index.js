9c0d957b290bb3af14579e68744a8f46aa3fdecb
Sachin <sachinmondal720@gmail.com>
2023-03-01 17:39:02 +0530

    adding friendList

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import App from './App'
 import Loader from './Loader'
 import NavBar from './NavBar'
+import FriendsList from './FriendList'
 export {
     App,
     Loader,
     NavBar,
+    FriendsList,
 }
